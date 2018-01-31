349d9fcf63dbaeba807d357d5c6cc23d7aa674d7
Kenya <win10.kesku@gmail.com>
2018-01-31 17:17:45 +0900

    add a function to rotate on sphere

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,29 +4,33 @@ using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour {
+    [SerializeField] Transform targetTrans;
+    [SerializeField, Range(1, 20)] float moveSpeed = 1f;
+    Camera camera;
+    Transform parent;
+    bool played = false;
     Vector3 initPos; //初期位置
     float viewingAngle; //視野角 ズームイン/アウトで使用
-    Camera camera;
-    bool played = false;
-    [SerializeField] Transform targetTrans;
+    Vector3 centerPos;
+
 
 	// Use this for initialization
 	void Start () {
+        parent = transform.parent;
         camera = GetComponent<Camera>();
         Debug.Log(camera.transform.position);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     public void StartControl() {
+        centerPos = GetCenterPos();
         initPos = transform.position;
         viewingAngle = camera.fieldOfView;
     }
     public void FinishControl() {
+        parent.position = initPos;
+        parent.rotation = Quaternion.Euler(0, 0, 0);
         transform.position = initPos;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
         camera.fieldOfView = viewingAngle;
     }
 
@@ -36,14 +40,19 @@ public class CameraController : MonoBehaviour {
         camera.fieldOfView = amount;
     }
 
-    //移動
-    //移動量
-    public void TranslateBySphere(Vector2 movement) {
-        ;
+    /// <summary>
+    /// 球面上に移動
+    /// </summary>
+    /// <param name='dragVec'>移動ベクトル</param>
+    public void TranslateOnSphere(Vector3 dragVec) {
+        var horizontal = Vector3.up * dragVec.x;
+        parent.RotateAround(centerPos, horizontal, moveSpeed);
+        var vertical = transform.right * dragVec.y;
+        transform.RotateAround(centerPos, vertical, moveSpeed);
     }
 
     public Vector3 GetCenterPos() {
-        return Vector3.zero;
+        return targetTrans.position;
     }
 
     public void ChangeState() {
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField] CameraController cameraController;
+    [SerializeField] RectTransform tapRect;
+    Vector3 beginTapPos;
+    Vector3 nowTapPos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TapAction();	
+	}
+
+    void TapAction() {
+        switch(AppUtil.GetTouch()) {
+            case TouchInfo.Began:
+                beginTapPos = AppUtil.GetTouchPosition();
+                tapRect.position = beginTapPos;
+            break;
+            case TouchInfo.Moved:
+                nowTapPos = AppUtil.GetTouchPosition();
+                var normalizedVec = AppUtil.GetDragDirection(beginTapPos, nowTapPos);
+                var translatedVec = new Vector3(normalizedVec.x, normalizedVec.y, 0);
+                if (Mathf.Abs(translatedVec.x) > Mathf.Abs(translatedVec.y))
+                    translatedVec.y = 0;
+                else translatedVec.x = 0;
+                //Debug.Log("vec:"+translatedVec);
+                cameraController.TranslateOnSphere(translatedVec);
+            break;
+            case TouchInfo.Ended:
+            break;
+        }
+    }
+}
